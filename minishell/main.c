b84f87676b0aa60c3f27131977c41b68bf6a57b1
uru <muru@mail.ru>
2021-10-20 17:37:25 +0300

    Initial Commit

---
+++ minishell/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  minishell
+//
+//  Created by Mikhail Uryvaev on 20.10.2021.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+	return 0;
+}
